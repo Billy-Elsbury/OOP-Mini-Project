d00ee6cefc56e427830a0901bd17317e747e9504
billy <billy.elsbury@students.ittralee.ie>
2022-11-16 20:03:12 +0000

    Made the writing and reading of txt file to login page more dynamic, 16-11-2022
    token: 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

--- RestaurantTillSystem/LoginPage.java
+++ RestaurantTillSystem/LoginPage.java
@@ -13,6 +13,7 @@ public class LoginPage extends JFrame{
     JButton btnClear;
     JLabel lbLogin;
     private JButton btnExit;
+    private JLabel lbInput;
     JFrame login;
 
 
@@ -32,30 +33,33 @@ public class LoginPage extends JFrame{
         login.setLocationRelativeTo(null);
         login.setSize(500, 500);
         login.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        login.pack();
         login.setVisible(true);
 
         btnLogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //retrieve login information from file
-                File outputFile = new File("loginDetails.txt");
+                File outFile = new File("loginDetails.txt");
 
                 try {
+                    FileOutputStream outputStream = new FileOutputStream(outFile);
 
-                    FileOutputStream outputStream = new FileOutputStream(outputFile);
-
-                    String loginDetails = "BillyElsbury T00224562";
+                    String setLoginName = "BillyElsbury";
 
                     ObjectOutputStream objectOutStream = new
                             ObjectOutputStream(outputStream);
 
+                    objectOutStream.writeObject(setLoginName);
+
                     outputStream.close();
 
-                } catch (FileNotFoundException ex) {
-                    throw new RuntimeException(ex);
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
+                } catch(FileNotFoundException fnfe){
+                    System.out.println(fnfe.getStackTrace());
+                    JOptionPane.showMessageDialog(null,"File could not be found!",
+                            "Problem Finding File!",JOptionPane.ERROR_MESSAGE);
+                } catch(IOException ioe){ System.out.println(ioe.getStackTrace());
+                    JOptionPane.showMessageDialog(null,"File could not be written!",
+                            "Problem Writing to File!",JOptionPane.ERROR_MESSAGE);
                 }
 
 
@@ -64,25 +68,42 @@ public class LoginPage extends JFrame{
                 try {
                     FileInputStream inputStream = new FileInputStream(loginFile);
 
-                    ObjectInputStream stringInSteam = new ObjectInputStream(inputStream);
+                    ObjectInputStream objectInStream = new ObjectInputStream(inputStream);
 
-                    String loginDetails = "BillyElsbury";
-
-                    System.out.println(loginDetails);
+                    System.out.println(objectInStream.readObject());
 
                     inputStream.close();
 
-                } catch (FileNotFoundException ex) {
-                    throw new RuntimeException(ex);
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+                } catch(FileNotFoundException fnfe){
+                    fnfe.printStackTrace();
+                    JOptionPane.showMessageDialog(null,"File could not be found!",
+                            "Problem Finding File!", JOptionPane.ERROR_MESSAGE);
+                } catch(IOException ioe){
+                    ioe.printStackTrace();
+                    JOptionPane.showMessageDialog(null,"File could not be read!",
+                            "Problem Writing to File!",JOptionPane.ERROR_MESSAGE);
+                } catch (ClassNotFoundException cnfe) {
+                    cnfe.printStackTrace();
+                    JOptionPane.showMessageDialog(null,"Could not convert object to"
+                            + " the appropriate class!","Problem Converting Object Read From File!",JOptionPane.ERROR_MESSAGE);
+
+                } catch (ClassCastException cce) {
+                    cce.printStackTrace();
+                    JOptionPane.showMessageDialog(null,"Could not convert the object to"
+                        + "the appropriate class!","Problem Converting Object!",JOptionPane.ERROR_MESSAGE); }
 
                 String loginName = tfName.getText();
                 String pinCode = tfPinCode.getText();
+                lbInput.setText("You inputted = Name: " + loginName
+                        + " Pin: " + pinCode);
+
+               if(loginName.equals("BillyElsbury")){
+                System.out.println("Amy is silly");
+                }
+
                 HomePage homePage = new HomePage();
-                homePage.setVisible(true);
-                login.setVisible(true);
+                //homePage.setVisible(true);
+                login.setVisible(false);
             }});
 
         btnClear.addActionListener(new ActionListener() {
