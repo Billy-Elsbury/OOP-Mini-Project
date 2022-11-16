fea68740af3f9ca851e53ae2fc33c8982ca75251
billy <billy.elsbury@students.ittralee.ie>
2022-11-16 17:39:25 +0000

    Added basic writing and reading of txt file to login page, 16-11-2022
    token: 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

--- RestaurantTillSystem/LoginPage.java
+++ RestaurantTillSystem/LoginPage.java
@@ -3,6 +3,7 @@ package RestaurantTillSystem;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 public class LoginPage extends JFrame{
     JPanel loginPanel;
@@ -11,31 +12,77 @@ public class LoginPage extends JFrame{
     JButton btnLogin;
     JButton btnClear;
     JLabel lbLogin;
+    private JButton btnExit;
     JFrame login;
 
 
-    public LoginPage() {
+    public LoginPage(){
         login = new JFrame("Login Frame");
+
+        /*****************************************************
+         * Icon image obtained from
+         * Site: https://icon-icons.com
+         * (Accessed 16 November 2022)
+         * *****************************************************/
+
+        login.setIconImage(new ImageIcon(this.getClass().getResource("loginIconImage.png")).getImage());
         login.setContentPane(loginPanel);
         login.setTitle("Till Login");
+        lbLogin.setText("Valid details = Name: BillyElsbury Pin: T00224562");
         login.setLocationRelativeTo(null);
-        login.setSize(1500, 450);
+        login.setSize(500, 500);
         login.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         login.pack();
         login.setVisible(true);
 
-
-        //setIconImage(new ImageIcon(getClass().getResource("iconImage.png")).getImage());
-
         btnLogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //retrieve login information from file
+                File outputFile = new File("loginDetails.txt");
+
+                try {
+
+                    FileOutputStream outputStream = new FileOutputStream(outputFile);
+
+                    String loginDetails = "BillyElsbury T00224562";
+
+                    ObjectOutputStream objectOutStream = new
+                            ObjectOutputStream(outputStream);
+
+                    outputStream.close();
+
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+
+                File loginFile = new File("loginDetails.txt");
+
+                try {
+                    FileInputStream inputStream = new FileInputStream(loginFile);
+
+                    ObjectInputStream stringInSteam = new ObjectInputStream(inputStream);
+
+                    String loginDetails = "BillyElsbury";
+
+                    System.out.println(loginDetails);
+
+                    inputStream.close();
+
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+
                 String loginName = tfName.getText();
                 String pinCode = tfPinCode.getText();
-                lbLogin.setText("Welcome: " + loginName + " " + pinCode);
                 HomePage homePage = new HomePage();
                 homePage.setVisible(true);
-                login.setVisible(false);
+                login.setVisible(true);
             }});
 
         btnClear.addActionListener(new ActionListener() {
@@ -44,6 +91,13 @@ public class LoginPage extends JFrame{
                 tfName.setText("");
                 tfPinCode.setText("");
             }});
+
+        btnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
     }
 
     public static void main(String[] args) {
